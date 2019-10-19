c0e7ea78ecc4f9f02ff44b910eba990106273ca3
Veykril <lukastw97@gmail.com>
2019-10-19 23:37:32 +0200

    add basic help descriptions to the cli app

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,78 @@ use std::str;
 #[derive(StructOpt)]
 #[structopt(author)]
 struct Opt {
-    #[structopt(parse(from_os_str))]
+    #[structopt(parse(from_os_str), help = "the input image to sort")]
     input: PathBuf,
 
-    #[structopt(short, default_value = "row")]
+    #[structopt(
+        short,
+        long = "interval",
+        default_value = "full",
+        help = "Interval function used to seperate the image into intervals.",
+        help = "Interval function used to seperate the image into intervals.\n\
+                available functions: full, edges, random, threshold"
+    )]
     interval_function: IntervalFunction,
 
-    #[structopt(short, long, parse(from_os_str))]
+    #[structopt(
+        short,
+        long,
+        parse(from_os_str),
+        help = "A file path to save the output image to."
+    )]
     output: Option<PathBuf>,
-    #[structopt(short, long, parse(from_os_str))]
+    #[structopt(
+        short,
+        long,
+        parse(from_os_str),
+        help = "A file path to a gray image to mask parts of the input image.",
+        long_help = "A file path to a gray image to mask parts of the input image.\n\
+                     White pixels may be sorted, black pixels may not."
+    )]
     mask: Option<PathBuf>,
 
-    #[structopt(short = "u", default_value = "1.0")]
+    #[structopt(
+        short = "u",
+        default_value_if("interval_function", Some("full"), "0.0"),
+        help = "The upper threshold used by some interval functions.",
+        long_help = "The upper threshold used by some interval functions.\n\
+                     \n\
+                     Required by `edge` in the range of [0.0;1140.39), accepts floating point numbers.\n\
+                     Required by `random`, defines the maximum possible size of the random intervals in integers.\n\
+                     Required by `threshold`, defines the upper threshold a pixels lightness has to fall below to be sorted."
+    )]
     upper: f32,
-    #[structopt(short = "l", default_value = "0.0")]
+    #[structopt(
+        short = "l",
+        default_value_if("interval_function", Some("full"), "0.0"),
+        help = "The lower threshold used by some interval functions.",
+        long_help = "The lower threshold used by some interval functions.\n\
+                     \n\
+                     Required by `edge` in the range of [0.0;1140.39), accepts floating point numbers.\n\
+                     Required by `random`, defines the minimum possible size of the random intervals in integers.\n\
+                     Required by `threshold`, defines the lower threshold a pixels lightness has to surpass to be sorted."
+    )]
     lower: f32,
-    #[structopt(short, long, default_value = "0")]
+    #[structopt(
+        short,
+        long,
+        default_value = "0",
+        help = "The rotation to apply to the image prior sorting.",
+        long_help = "The rotation to apply to the image prior sorting.\n\
+                     \n\
+                     This value defines the angle at which pixels will be sorted. This may be any multiple of 90 degrees.\n\
+                     To sort vertically instead of horizontally for example one would specifiy a rotation of 90 or 270 degrees."
+    )]
     rotate: Rotation,
-    #[structopt(short, long, default_value = "lightness")]
+    #[structopt(
+        short,
+        long,
+        default_value = "lightness",
+        help = "The function to use for sorting pixels.",
+        long_help = "The function to use for sorting pixels.\n\
+                     \n\
+                     This mode defines how pixels are sorted, be it by lightness, intensity or min/maxmimum channel value of each pixel."
+    )]
     sorting: SortingMode,
 }
 
@@ -76,19 +130,23 @@ pub enum Rotation {
 impl str::FromStr for Rotation {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "0" | "360" => Ok(Rotation::Zero),
-            "90" => Ok(Rotation::Quarter),
-            "180" => Ok(Rotation::Half),
-            "270" | "-90" => Ok(Rotation::NegQuarter),
-            _ => Err(String::from(s)),
+        let num = s
+            .parse::<isize>()
+            .map_err(|e| format!("{:?}", e))?
+            .rem_euclid(360);
+        match num {
+            0 => Ok(Rotation::Zero),
+            90 => Ok(Rotation::Quarter),
+            180 => Ok(Rotation::Half),
+            270 => Ok(Rotation::NegQuarter),
+            _ => Err(String::from("rotation angle must be a multiple of 90")),
         }
     }
 }
 
 #[derive(Clone, Copy)]
 pub enum IntervalFunction {
-    Row,
+    Full,
     #[cfg(feature = "imageproc")]
     Edges,
     #[cfg(feature = "rand")]
@@ -100,7 +158,7 @@ impl str::FromStr for IntervalFunction {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "row" => Ok(IntervalFunction::Row),
+            "full" => Ok(IntervalFunction::Full),
             #[cfg(feature = "imageproc")]
             "edge" => Ok(IntervalFunction::Edges),
             #[cfg(feature = "rand")]
@@ -140,7 +198,7 @@ fn main() {
     }
 
     match opt.interval_function {
-        IntervalFunction::Row => (),
+        IntervalFunction::Full => (),
         #[cfg(feature = "imageproc")]
         IntervalFunction::Edges => {
             interval::edges_canny(&mut intervals, &image, opt.lower, opt.upper)
